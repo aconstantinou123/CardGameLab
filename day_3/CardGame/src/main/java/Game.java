bb44654e6b639e9b52df26ceeebf4e0c84aa4484
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-12-06 14:47:56 +0000

    game class created

--- day_3/CardGame/src/main/java/Game.java
+++ day_3/CardGame/src/main/java/Game.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Game {
+
+    private Deck deck;
+    private ArrayList<Player> players;
+
+    public Game(Deck deck) {
+        this.deck = deck;
+        this.players = new ArrayList<>();
+    }
+
+    public int playerCount() {
+        return this.players.size();
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
 }
---
+++ day_3/CardGame/src/test/java/GameTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    Deck deck1;
+    Deck deck2;
+    Card card1;
+    Hand hand1;
+    Hand hand2;
+    Player player1;
+    Player player2;
+    Game game1;
+
+    @Before
+    public void before() {
+        card1 = new Card(SuitType.SPADES, CardValue.ACE);
+        deck1 = new Deck();
+        deck2 = new Deck();
+        hand1 = new Hand();
+        hand2 = new Hand();
+        player1 = new Player("Player1", hand1);
+        player2 = new Player("Player2", hand2);
+        game1 = new Game(deck1);
+        game1.addPlayer(player1);
+        game1.addPlayer(player2);
+
+    }
+
+    @Test
+    public void canAddPlayer() {
+        assertEquals(2, game1.playerCount());
+    }
+}
